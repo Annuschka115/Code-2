7d13852ce6d1d98da1a8e57dfdc87b6f686d4d78
Annu <anne.ploch@gmx.de>
2025-10-24 16:49:51 +0200

    canvas

--- Canvas/Canvas.js
+++ Canvas/Canvas.js
@@ -1,18 +1,9 @@
 "use strict";
-window.addEventListener('load', (_event) => { console.log("has loaded"); });
-const canvas = document.querySelector("canvas");
-const crc2 = canvas.getContext("2d");
-crc2.fillStyle = "#201787ff";
-crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-crc2.beginPath();
-crc2.arc(crc2.canvas.width / 2, crc2.canvas.height / 2, 90, 0, 10);
-crc2.closePath();
-crc2.stroke();
-const path = new Path2D();
-// path.moveTo(crc2.canvas.width / 2, crc2.canvas.height / 2);
-crc2.beginPath();
-crc2.lineTo(crc2.canvas.width / 2, crc2.canvas.height / 2);
-crc2.lineTo(crc2.canvas.width / 2 + 90, crc2.canvas.height / 2);
-crc2.closePath();
-crc2.stroke();
-console.log("AAAAAAAAaaa");
+var Canvas;
+(function (Canvas) {
+    window.addEventListener('load', (_event) => { console.log("has loaded"); });
+    const canvas = document.querySelector("canvas");
+    const crc2 = canvas.getContext("2d");
+    function doTriangle(vectorX, vectorY) {
+    }
+})(Canvas || (Canvas = {}));
--- Canvas/Canvas.ts
+++ Canvas/Canvas.ts
@@ -1,8 +1,13 @@
+namespace Canvas {
 window.addEventListener ('load',(_event:Event)=>{console.log("has loaded")});
 
 const canvas: HTMLCanvasElement = document.querySelector("canvas") as HTMLCanvasElement;
 const crc2: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
 
+
+
+
+/*
 crc2.fillStyle= "#201787ff";
 crc2.fillRect(0,0,crc2.canvas.width, crc2.canvas.height);
 crc2.beginPath();
@@ -11,7 +16,7 @@ crc2.closePath();
 crc2.stroke();
 
 
-const path: Path2D = new Path2D();
+
 // path.moveTo(crc2.canvas.width / 2, crc2.canvas.height / 2);
 crc2.beginPath();
 crc2.lineTo(crc2.canvas.width / 2, crc2.canvas.height / 2);
@@ -19,5 +24,17 @@ crc2.lineTo(crc2.canvas.width / 2+90, crc2.canvas.height / 2);
 crc2.closePath();
 crc2.stroke();
 
+crc2.beginPath();
+crc2.strokeText("owo",crc2.canvas.width / 2, crc2.canvas.height / 2,  20000000000000);
+crc2.closePath();
 console.log("AAAAAAAAaaa");
 
+*/
+
+
+type Vector = {x:number,y:number}
+
+function doTriangle(vectorX:number,vectorY:number):void{
+
+}
+}
