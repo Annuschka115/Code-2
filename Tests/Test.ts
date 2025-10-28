8993d85ca035f2a2b7e77f7daac673c9c210061d
Annu <anne.ploch@gmx.de>
2025-10-28 15:15:19 +0100

    Geistige Verwirrung

---
+++ Tests/Test.ts
@@ -0,0 +1,46 @@
+namespace HUH {
+
+// class Vector {
+// x: number = 2;
+// y: number = 4 ;
+
+// constructor (_x:number , _y:number) {
+//     this.set(_x , _y);
+// }
+//     set (_x: number, _y:number ): void{
+//         this.x = _x;
+//         this.y = _y; 
+//     }
+//     scale(_factor: number): void {
+//         this.x *= _factor;
+//         this.y *= _factor;
+//     }
+
+//     add(_addend: Vector): void {
+//         this.x += _addend.x;
+//         this.y += _addend.y;
+
+//     }
+  
+// }
+
+
+// const v1: Vector = new Vector( 3, 6);
+// // v1.set(0,0);
+// v1.scale(2);
+// console.log(v1);
+
+
+enum Animals {
+    CAT,
+    DOG,
+    COW,
+    PIG,
+    CHICKEN
+}
+
+
+
+
+
+}
